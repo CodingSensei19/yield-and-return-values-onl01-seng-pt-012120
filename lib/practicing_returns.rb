346f1795d4084072768e509ddcc3b9402d8e5b74
Aaron Wilson <codingsensei19@gmail.com>
2020-02-02 21:43:36 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,40 +1,12 @@
-def yielding_with_arguments(num)
-  puts "the program is executing the code inside the method"
-  yield(num)
-  puts "now we are back in the method"
-end
+require 'pry'
 
-yielding_with_arguments(2) {|i| puts i * 2}
-
-
-############## FOLLOWING METHOD WILL BREAK IF NOT CALLED WITH A BLOCK
-
-def hello_t(array)
+def hello(array)
   i = 0
   while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-  array
-end
-
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-    if name.start_with?("T")
-        puts "Hi, #{name}"
-    end
-end
-
-################# FOLLOWING METHOD ALLOWS FOR CALLING WITH OR WITHOUT BLOCK
-
-def hello_t(array)
-  if block_given?
-    i = 0
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
-    array
-  else
-    puts "Hey! No block was given!"
+    yield(array[i])
+    i += 1
   end
 end
+
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
